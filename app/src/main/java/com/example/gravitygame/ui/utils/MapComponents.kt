6771a5df07f6e9aa956ebd830701f3cc27adf150
Marek <marek.vachal@gmail.com>
2024-08-22 21:31:57 +0200

    Basic source code

--- app/src/main/java/com/example/gravitygame/ui/utils/MapComponents.kt
+++ app/src/main/java/com/example/gravitygame/ui/utils/MapComponents.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
